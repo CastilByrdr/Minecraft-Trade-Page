491169461ce9c239df3e9381d52d6726e90d0528
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-25 13:24:10 -0400

    add files

---
+++ front-end/src/views/About.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h3 class="title">About Page</h3>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+     <div class="container">
+    <h3 class="title">Login Page</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
