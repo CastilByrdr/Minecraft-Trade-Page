2baa963f3b085b1559f740d6177892a680cb93eb
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-27 19:31:14 -0400

    login and signup page

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,13 +1,118 @@
 <script setup lang="ts">
+import router from "@/router";
+import { authState } from "@/state/user";
 
+let username = "";
+let password = "";
+
+function login() {
+  if (!username) {
+    throw new Error("Please provide username");
+  }
+
+  if (!password) {
+    throw new Error("Please provide password");
+  }
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
+
+  loginApi(username);
+}
+
+function loginApi(username: string) {
+  console.log("Logging in...");
+
+  console.log("Logging successfully");
+  authState.username.value = username;
+  router.push("/");
+
+  // setTimeout(() => {
+  //   console.log("Logging successfully");
+  //   authState.username.value = username;
+  //   router.push('/');
+  // }, 3000);
+}
 </script>
 
 <template>
-     <div class="container">
-    <h3 class="title">Login Page</h3>
+  <div class="login">
+    <h1 class="title">Login Page</h1>
+    <div class="container is-fluid">
+      <div class="columns is-justify-content-center">
+        <div
+          class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+        >
+          <div class="card">
+            <div class="card-content">
+              <div class="title mt-2 has-text-centered">Sign In</div>
+
+              <div class="field">
+                <label class="label">Username</label>
+                <div class="control has-icons-left">
+                  <input
+                    class="input"
+                    type="text"
+                    placeholder="Username"
+                    v-model="username"
+                  />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                  </span>
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Password</label>
+                <div class="control has-icons-left">
+                  <input
+                    class="input"
+                    type="password"
+                    placeholder="Password"
+                    v-model="password"
+                  />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                  </span>
+                </div>
+              </div>
+
+              <div class="field">
+                <div class="control is-flex">
+                  <button
+                    class="button is-warning is-flex-grow-1"
+                    @click="login"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </div>
+
+              <div class="field">
+                <div class="control">
+                  <label class="checkbox">
+                    Need to register? Create an account
+                    <a href="./register">here</a>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.login {
+    font-family: 'Courier New', Courier, monospace;
+}
+
+.control is-flex {
+    background-color: #267831;
+    font-size: 16px;
+    transition: none;
+}
 
 </style>
