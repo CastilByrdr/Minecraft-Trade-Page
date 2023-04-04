4f86e6a6d424f19f72048376f0ddd3ffb99c56f2
gfzhunio <90656820+gfzhunio@users.noreply.github.com>
2023-04-04 12:15:20 -0400

    add user table

---
+++ src/views/Users.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts"></script>
+
+<template>
+  
+  <table class="table is-bordered is-striped is-narrow is-hoverable">
+    <thead>
+        <tr>
+            <th>User ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Last login</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>karoz</td>
+            <td>karo@hotmail.com</td>
+            <td>123</td>
+            <td>12/30/2022</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>michi</td>
+            <td>michi@hotmail.com</td>
+            <td>456</td>
+            <td>11/30/2022</td>
+        </tr>
+    </tbody>
+  </table>
+  
+</template>
+
+<style scoped>
+
+ table, th, td {
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+ }
+
+
+</style>
