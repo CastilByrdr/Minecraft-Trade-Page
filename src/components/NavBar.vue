d39e6264a6655b15155fd6da3d42cc7c0c7188af
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-08 22:50:33 -0400

    modify login and register page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="navbar is-transparent">
+    <a>
+          <img
+            id="title"
+            src="graphics/HeaderTitle.png"
+            alt="Minecraft Marketplace: The online item-trading hub for Minecraft servers!"
+            class="center"
+          />
+        </a>
+    
+    <div class="navbar-brand">
+      <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+        
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item is-primary"> Home </a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a
+            class="navbar-link"
+            href="https://bulma.io/documentation/overview/start/"
+          >
+            Docs
+          </a>
+          <div class="navbar-dropdown is-boxed">
+            <a
+              class="navbar-item"
+              href="https://bulma.io/documentation/overview/start/"
+            >
+              Overview
+            </a>
+            <a
+              class="navbar-item"
+              href="https://bulma.io/documentation/overview/modifiers/"
+            >
+              Modifiers
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <p class="control">
+              <a
+                class="button has-text-black"
+                href=""
+              >
+                <span class="icon">
+                  <i class="fa-solid fa-user-plus"></i>
+                </span>
+                <span>Register</span>
+              </a>
+            </p>
+            <p class="control">
+              <a
+                class="button is-primary"
+                href=""
+              >
+                <span class="icon">
+                  <i class="fa-solid fa-right-to-bracket"></i>
+                </span>
+                <span>Login</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: rgb(105, 172, 235);
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import router from "@/router";
 import { authState } from "@/state/user";
+import NavBar from "@/components/NavBar.vue";
 
-let username = "";
-let password = "";
+let username = "mzhunio";
+let password = "12345";
 
 function login() {
   if (!username) {
@@ -36,77 +37,82 @@ function loginApi(username: string) {
 </script>
 
 <template>
-    <div class="container is-fluid">
-      <div class="columns is-justify-content-center">
-        <div
-          class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
-        >
-          <div class="card">
-            <div class="card-content">
-              <div class="title mt-2 has-text-centered">Sign In</div>
+  <NavBar />
 
-              <div class="field">
-                <label class="label">Username</label>
-                <div class="control has-icons-left">
-                  <input
-                    class="input"
-                    type="text"
-                    placeholder="Username"
-                    v-model="username"
-                  />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                  </span>
-                </div>
+  <div class="container is-fluid mt-5">
+    <div class="columns is-justify-content-center">
+      <div
+        class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+      >
+        <div class="card">
+          <div class="card-content">
+            <div class="title mt-2 has-text-centered">Sign In</div>
+
+            <div class="field">
+              <label class="label">Username</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Username"
+                  v-model="username"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
               </div>
+            </div>
 
-              <div class="field">
-                <label class="label">Password</label>
-                <div class="control has-icons-left">
-                  <input
-                    class="input"
-                    type="password"
-                    placeholder="Password"
-                    v-model="password"
-                  />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                  </span>
-                </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="password"
+                  placeholder="Password"
+                  v-model="password"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
               </div>
+            </div>
 
-              <div class="field">
-                <div class="control is-flex">
-                  <button
-                    class="button is-warning is-flex-grow-1"
-                    @click="login"
-                  >
-                    Sign in
-                  </button>
-                </div>
+            <div class="field">
+              <div class="control is-flex">
+                <button
+                  class="button is-success is-light is-flex-grow-1"
+                  @click="login"
+                  type="button"
+                >
+                  Sign in
+                </button>
               </div>
+            </div>
 
-              <div class="field">
-                <div class="control">
-                  <label class="checkbox">
-                    Need to register? Create an account
-                    <a href="./register">here</a>
-                  </label>
-                </div>
+            <div class="field">
+              <div class="control">
+                <label class="checkbox">
+                  Need to register? Create an account
+                  <a href="./register">here</a>
+                </label>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+  </div>
 </template>
 
 <style scoped>
 .control is-flex {
-    background-color: #267831;
-    font-size: 16px;
-    transition: none;
+  font-size: 16px;
+  transition: none;
 }
 
+
+.button {
+  box-shadow: 3px 3px #42a168;
+}
 </style>
