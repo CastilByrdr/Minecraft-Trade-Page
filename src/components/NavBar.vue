67ccc057b6022f455910bee7c873aab897c60998
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-09 10:26:41 -0400

    add functions to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,61 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from "@/router";
+import { computed, ref } from "vue";
+import { authState } from "../state/user";
+
+const isMenuActive = ref(false);
+const isUserLoggedIn = computed(() => !!authState.username.value);
+
+function onLogout(): void {
+  logOutApi();
+}
+
+function logOutApi() {
+  console.log("Logging out...");
+
+  setTimeout(() => {
+    console.log("Logged out successfully");
+    authState.username.value = "";
+    router.push("/login");
+  }, 3000);
+}
+</script>
 
 <template>
   <nav class="navbar is-justify-content-center pt-2" role="navigation">
-    <img
-      id="title"
-      src="graphics/HeaderTitle.png"
-      alt="Minecraft Marketplace: The online item-trading hub for Minecraft servers!"
-      class="center"
-    />
+    <router-link to="./">
+      <img
+        id="title"
+        src="graphics/HeaderTitle.png"
+        alt="Minecraft Marketplace: The online item-trading hub for Minecraft servers!"
+        class="navbar-item center"
+      />
+    </router-link>
   </nav>
   <nav class="navbar is-transparent">
     <div class="navbar-brand">
-      <div
-        class="navbar-burger"
-        data-target="navbarExampleTransparentExample"
-      ></div>
+      <div class="navbar-burger" data-target="navbar"></div>
     </div>
 
-    <div id="navbarExampleTransparentExample" class="navbar-menu">
-      <!-- <div class="navbar-start">
-        <a class="navbar-item is-primary"> Home </a>
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a
-            class="navbar-link"
-            href="https://bulma.io/documentation/overview/start/"
-          >
-            Docs
-          </a>
-          <div class="navbar-dropdown is-boxed">
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/overview/start/"
-            >
-              Overview
-            </a>
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/overview/modifiers/"
-            >
-              Modifiers
-            </a>
-          </div>
-        </div>
-      </div> -->
-
+    <div id="navbar" class="navbar-menu" :class="{ 'is-active': isMenuActive }">
       <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
+        <router-link to="/home" class="navbar-item">
+          <span>Home</span>
+        </router-link>
+        <router-link to="/index" class="navbar-item">
+          <span>Index</span>
+        </router-link>
 
         <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
+          <a class="navbar-link"> Search </a>
 
           <div class="navbar-dropdown">
             <a class="navbar-item"> About </a>
             <a class="navbar-item"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
             <hr class="navbar-divider" />
             <a class="navbar-item"> Report an issue </a>
           </div>
@@ -66,20 +62,34 @@
         <div class="navbar-item">
           <div class="field is-grouped">
             <p class="control">
-              <a class="button has-text-black" href="">
+              <button v-if="isUserLoggedIn" class="button">
+                Welcome {{ authState.username }}
+              </button>
+              <button v-if="isUserLoggedIn" class="button" @click="onLogout()">
+                Log Out
+              </button>
+              <router-link
+                v-if="!isUserLoggedIn"
+                to="/register"
+                class="button has-text-black"
+              >
                 <span class="icon">
                   <i class="fa-solid fa-user-plus"></i>
                 </span>
                 <span>Register</span>
-              </a>
+              </router-link>
             </p>
             <p class="control">
-              <a class="button is-primary" href="">
+              <router-link
+                v-if="!isUserLoggedIn"
+                to="/login"
+                class="button is-primary"
+              >
                 <span class="icon">
                   <i class="fa-solid fa-right-to-bracket"></i>
                 </span>
                 <span>Login</span>
-              </a>
+              </router-link>
             </p>
           </div>
         </div>
