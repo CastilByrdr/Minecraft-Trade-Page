2a2e1477cdd67593be9e233ffedbc66af6a79b01
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-10 18:42:52 -0400

    added List of foods

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
-
-const foods = [
-  { foodName: 'sadfsad', imageSrc: '/subcategories/food/cooked-food/cooked-chicken.png' },
-  { foodName: 'sadfsad', imageSrc: '/subcategories/food/cooked-food/cooked-cod.png' },
-];
 </script>
 
 <template>
   <NavBar />
-
-  <div v-for="food in foods">
-    <div>{{ food.foodName }}</div>
-    <img :src="food.imageSrc" :alt="food.foodName">
-  </div>
 
   <main>
     <RouterView />
